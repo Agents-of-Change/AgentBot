43ea508123c4e37216a743ccb749a9231126481d
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2022-12-26 21:34:36 -0800

    One-on-ones join and leave

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import discord
-from .config import TOKEN, GUILD_ID
+from config import TOKEN, GUILD_ID
+from db import sqlite3, db
 
 intents = discord.Intents.default()
 # intents.message_content = True
@@ -16,6 +17,43 @@ async def on_ready():
 async def hello(ctx):
     await ctx.respond("Hello!")
 
+
+async def one_on_ones_join(ctx):
+    db.execute(
+        """
+            INSERT INTO users (discordId, matchable) 
+                VALUES (?, TRUE)
+            ON CONFLICT(discordId) DO UPDATE SET
+                matchable = TRUE
+        """, 
+        (str(ctx.author.id), )
+    )
+    db.commit()
+    await ctx.respond("Done!")
+
+async def one_on_ones_leave(ctx):
+    db.execute(
+        """
+            UPDATE users SET matchable = FALSE WHERE discordId = ?
+        """,
+        (str(ctx.author.id), )
+    )
+    db.commit()
+    await ctx.respond("Done!")
+
+
+@bot.slash_command(guild_ids=[GUILD_ID])
+async def one_on_ones(
+    ctx,
+    action: discord.Option(
+        input_type=discord.SlashCommandOptionType.sub_command, choices=["join", "leave"]
+    ),
+):
+    if action == "join":
+        return await one_on_ones_join(ctx)
+    if action == "leave":
+        return await one_on_ones_leave(ctx)
+    await ctx.respond("Invalid option")
 
 
 bot.run(TOKEN)
