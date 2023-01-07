5bc7323c1b742a5c0ff4ea387f7d798631ddab71
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-01-06 18:20:07 -0800

    Add database not setup protection

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from config import TOKEN, GUILD_ID
 from utils import *
+from one_on_ones import *
+from sqlite3 import OperationalError
 
 
 @bot.event
@@ -17,4 +19,16 @@ async def jump_to_introduction(ctx, member):
     await ctx.respond(f"Hello world! <@{member.id}>")
 
 
-bot.run(TOKEN)
+def main():
+    try:
+        db.execute("SELECT 1 FROM users")
+        db.execute("SELECT 1 FROM past_matches")
+    except OperationalError as e:
+        raise AssertionError(
+            "Error: DB not setup properly. Run scripts/db_setup.py"
+        ) from e
+    bot.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
