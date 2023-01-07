6f005b6314581274bda593de5362e5222fdd7cb0
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-01-06 18:22:05 -0800

    record_manual_pair impl + fixups

--- src/one_on_ones.py
+++ src/one_on_ones.py
@@ -118,16 +118,33 @@ def write_matches(matches):
     db.commit()
 
 
+class NotRegisteredError(Exception):
+    pass
+
+
+def fetch_discord_id(uid):
+    cur = db.cursor()
+    cur.execute("SELECT discordId FROM users WHERE id = ?", (uid,))
+    r = cur.fetchone()
+    if r is None:
+        raise NotRegisteredError(f"UID is not registered: {uid}")
+    return r[0]
+
+
 def fetch_many_discord_ids(uids):
     out = []
     for i in uids:
-        cur = db.cursor()
-        cur.execute("SELECT discordId FROM users WHERE id = ?", (i,))
-        r = cur.fetchone()
-        if r is None:
-            raise AssertionError(f"Invalid UID: {i}")
-        out.append(r[0])
+        out.append(fetch_discord_id(i))
     return out
+
+
+def fetch_user_id(disc_id):
+    cur = db.cursor()
+    cur.execute("SELECT id FROM users WHERE discordId = ?", (str(disc_id),))
+    r = cur.fetchone()
+    if r is None:
+        raise NotRegisteredError(f"Discord ID is not registered: {disc_id}")
+    return r[0]
 
 
 def matches_with_discord_ids(matches):
@@ -155,3 +172,45 @@ async def roll_one_on_ones(ctx):
         ]
     write_matches(matches)
     await ctx.send_response("\n".join(msg))
+
+
+def auto_register_user(disc_id):
+    cur = db.cursor()
+    cur.execute(
+        "INSERT INTO users (discordId, matchable) VALUES (?, FALSE) RETURNING id",
+        (disc_id,),
+    )
+    r = cur.fetchone()
+    db.commit()
+    return r[0]
+
+
+@guild_slash_command(
+    description="Record a 1-1 pairing manually. Invalid users will be automatically registered with matchable=False."
+)
+@admin_only
+async def record_manual_pair(
+    ctx,
+    user_a: discord.Option(input_type=discord.SlashCommandOptionType.user),
+    user_b: discord.Option(input_type=discord.SlashCommandOptionType.user),
+):
+    user_a_discord, user_b_discord = map(id_from_mention, (user_a, user_b))
+    db_ids = []
+    msgs = []
+    for disc_id in (user_a_discord, user_b_discord):
+        try:
+            db_id = fetch_user_id(disc_id)
+            msg = ""
+        except NotRegisteredError:
+            db_id = auto_register_user(disc_id)
+            msg = " (auto registered)"
+        db_ids.append(db_id)
+        msgs.append(msg)
+
+    user_a_id, user_b_id = db_ids
+    user_a_msg, user_b_msg = msgs
+    write_matches([(user_a_id, user_b_id)])
+    await ctx.send_response(
+        f":white_check_mark: Sucessfully paired {mention(user_a_discord)}{user_a_msg} and {mention(user_b_discord)}{user_b_msg}",
+        allowed_mentions=discord.AllowedMentions.none(),
+    )
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import discord
 import functools
+import re
 from config import ADMIN_ROLE_ID, GUILD_ID
 
 intents = discord.Intents.default()
@@ -25,3 +26,10 @@ def admin_only(func):
         return await func(ctx, *args, **kwargs)
 
     return decorated
+
+
+def id_from_mention(mention):
+    m = re.match(r"<@!?([0-9]{15,20})>$", mention)
+    if m is None:
+        raise ValueError(f"Value is not a mention: {mention!r}")
+    return int(m.group(1))
