8256a9c580582f6a7fee696cdab0352d08081510
Spencer Pogorzelski <34356756+Scoder12@users.noreply.github.com>
2023-03-06 21:53:15 -0800

    try get_event_loop

--- src/main.py
+++ src/main.py
@@ -62,6 +62,5 @@ async def main():
 
 
 if __name__ == "__main__":
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(main())
-    bot.run(TOKEN)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
